20cfbc135aa520ebcf33b761f24e68c4743258c3
Henry W <s.m.system@googlemail.com>
2024-12-02 13:40:03 +0100

    Admin dashboard created

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,3 +1,24 @@
 <x-app-layout>
-
+       <!-- Section-->
+       <section class="py-5">
+        <div class="container px-4 px-lg-5 mt-5">
+            <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                <div class="col mb-5">
+                    <div class="card h-100">
+                        <!-- Product image-->
+                        <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                        <!-- Product details-->
+                        <div class="card-body p-4">
+                            <div class="text-center">
+                                <!-- Product name-->
+                                <h5 class="fw-bolder">Fancy Product</h5>
+                                <!-- Product price-->
+                                $40.00
+                            </div>
+                        </div>
+      
+                    </div>
+                </div>
+            
+    </section>
 </x-app-layout>
